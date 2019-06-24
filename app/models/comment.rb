d3d8e3a966e1deb7f66e7ae1633ce8a42cd3dadc
jpier2012 <jpier2012@gmail.com>
2019-06-24 13:46:10 -0700

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,12 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  def users_attributes=(users)
+    users.each do |index, user_attributes|
+      if !user_attributes[:username].blank?
+        user = User.find_or_create_by(username: user_attributes[:username])
+        self.user = user
+      end
+    end
+  end
 end
