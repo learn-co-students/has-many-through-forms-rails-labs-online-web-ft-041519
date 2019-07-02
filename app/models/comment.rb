646208a1e1a979ae27ac3cafcdef5788e92d5833
Ashleigh Chun <ashleigh.chun@gmail.com>
2019-07-02 13:45:40 -0700

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
-class Comment < ActiveRecord::Base
+class Comment < ActiveRecord::Base #join table
   belongs_to :user
   belongs_to :post
-
+  accepts_nested_attributes_for :user, reject_if: :all_blank
 end
