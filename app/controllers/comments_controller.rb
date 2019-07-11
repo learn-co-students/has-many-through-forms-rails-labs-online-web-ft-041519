4f01482ca71bd3fb640ec906d2f9f663b372c464
Kish-Siva-1 <rodes88@msn.com>
2019-07-11 14:58:54 +0200

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,12 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
-  end
+    if !params[:comment][:user_attributes][:username].empty?
+      params.require(:comment).permit(:content, :post_id, user_attributes: [:username])
+    else 
+      params.require(:comment).permit(:content, :user_id, :post_id)
+    end
+
+  end 
+
 end
